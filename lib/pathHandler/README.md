f3058dbef8163b485f2b5e89eaa66c89b3d3bed1
Crazy-White <565332016@qq.com>
2020-10-04 22:33:30 +0800

    upgrade pathHandler

--- lib/pathHandler/README.md
+++ lib/pathHandler/README.md
@@ -12,15 +12,24 @@
 | /index.php/path/to/file?q=a | /path/to/file | /path/to/file?q=a |
 
 [^1]: /[index.php]?/path/to/file 表示 /?/path/to/file 或 /index.php?/path/to/file  
-并非一定要是index.php，但最好是。  
+其中，index.php也可以是其它脚本。  
   
 [^2]:  调用 \pathHandler\redirect() 函数后 /?/path/to/file?q=a 才会跳转至 /?/path/to/file&q=a  
 请注意 \pathHandler\get(1) 的区别。  
+  
+如需要伪静态，请这样设置：
+```
+// Apache
+RewriteRule ^(.*) index.php/$1 [L]
+// Nginx
+rewrite  ^(.*)$  /index.php/$1
+// index.php可以是其它脚本
+```
 
 ```php
 require_once("./lib/pathHandler/main.php");  // 引入库文件
 \pathHandler\redirect();  // 处理$_GET失效问题
-// 函数只调用一次！
+// 函数只需要（应当）调用一次！
 $path0 = \pathHandler\get(0);    // 不含querystring
 $path1 = \pathHandler\get(1);    // 包括querystring
 ```
--- lib/pathHandler/main.php
+++ lib/pathHandler/main.php
@@ -1,16 +1,25 @@
 <?php
+/**
+ * Then no difficulty in handling path.
+ *
+ * @author    CrazyWhite <moe@mailo.com>
+ * @copyright 2021 (c) CrazyWhite - pathHandler
+ * @license   https://opensource.org/licenses/MIT - The MIT License (MIT)
+ * @link      https://github.com/Crazy-White/unpkg-proxy/tree/master/lib/pathHandler
+ * @since     1.0.2
+ */
+
 namespace pathHandler;
 
 function redirect()
 {
     /*
-    'localhost/sn.php?/path/to/file?fakeQuery'
-    to
-    'localhost/sn.php?/path/to/file&fakeQuery'
+    'localhost/sn.php?/path/to/file?fakeQuery'  to  'localhost/sn.php?/path/to/file&fakeQuery'
     then $_GET works
     */
-    $uri   = $_SERVER['REQUEST_URI'];
+    $uri = $_SERVER['REQUEST_URI'];
     $query = $_SERVER['QUERY_STRING'];
+
     if (strpos($query, '?') > 0) {
         $fixed_query = str_replace('?', '&', $query);
         header('Location: ' . str_replace($query, $fixed_query, $uri));
@@ -20,32 +29,51 @@ function redirect()
 
 function get($is_include_query = false)
 {
-    $pi    = $_SERVER['PATH_INFO'];
-    $uri   = $_SERVER['REQUEST_URI'];
+    $pi = $_SERVER['PATH_INFO'];
+    $uri = $_SERVER['REQUEST_URI'];
     $query = $_SERVER['QUERY_STRING'];
-    
+    $sn = $_SERVER['SCRIPT_NAME'];
+
+
     if (isset($pi) && strlen($pi) > 0) {
-        
-        if (!$is_include_query)
+        /* for path_info&rewrite */
+        if (!$is_include_query) {
             return $pi;
-        //$_pi = str_replace('/', '\/', $pi);
-        $sn = addcslashes($_SERVER['SCRIPT_NAME'], '/');
-        if (preg_match("/{$sn}(.+)$/", $uri, $matches)) {
-            return $matches[1];
         }
-        
+
+        if (startsWith($uri, $sn)) {
+            return substr($uri, strlen($sn));
+        }
     }
-    
+
     if ($query[0] === '/') {
-        
+        /* for querystring */
         if ($is_include_query) {
             return $query;
         } else {
-            preg_match('/([\w+|\/]*)/', $query, $matches2);
-            return $matches2[1];
+            $p = strpos($query, '?');
+            if (!$p) {
+                $p = strpos($query, '&');
+                if (!$p) {
+                    return $query;
+                }
+            }
+            return substr($query, 0, $p);
         }
-        
     }
-    
+
+    if (isset($uri)) {
+        return $uri;
+    }
+
     return '/';
+}
+
+function startsWith($haystack, $needle)
+{
+    return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
+}
+function endsWith($haystack, $needle)
+{
+    return substr_compare($haystack, $needle, -strlen($needle)) === 0;
 }
